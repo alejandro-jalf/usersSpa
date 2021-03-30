e04b9fc57cc551689f6298b372ca9edea42dfed9
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-03-30 13:36:20 -0600

    ADD: Crea esquemas para validacion de datos

---
+++ src/schema/index.js
@@ -0,0 +1,69 @@
+const joi = require('joi');
+
+const schemas = (() => {
+    
+    const schemaContentLetters = joi.string().regex(/[a-zA-Z]+/);
+
+    const schemaContentNumbers = joi.string().regex(/\d+/);
+
+    const schemaCreateUser = joi.object({
+        nombre_user: joi.string().min(1).max(35).required(),
+        apellido_p_user: joi.string().min(1).max(35).required(),
+        apellido_m_user: joi.string().min(1).max(35).required(),
+        direccion_user: joi.string().min(1).max(150).required(),
+        sucursal_user: joi.string().min(1).max(25).required(),
+        correo_user: joi.string().min(1).max(70).email().required(),
+        password_user: joi.string().min(1).max(150).required(),
+        tipo_user: joi.string().min(1).max(10).allow('invited', 'manager', 'executive').required(),
+        access_to_user: joi.string().min(1).required()
+    });
+
+    const schemaUpdateUser = joi.object({
+        nombre_user: joi.string().min(1).max(35).required(),
+        apellido_p_user: joi.string().min(1).max(35).required(),
+        apellido_m_user: joi.string().min(1).max(35).required(),
+        direccion_user: joi.string().min(1).max(150).required(),
+        sucursal_user: joi.string().min(1).max(25).required(),
+        correo_user: joi.string().min(1).max(70).email().required(),
+        tipo_user: joi.string().min(1).max(10).allow('invited', 'manager', 'executive').required(),
+        access_to_user: joi.string().min(1).required(),
+        activo_user: joi.boolean().required()
+    });
+
+    const schemaUpdateDataGeneral = joi.object({
+        nombre_user: joi.string().min(1).max(35).required(),
+        apellido_p_user: joi.string().min(1).max(35).required(),
+        apellido_m_user: joi.string().min(1).max(35).required(),
+        direccion_user: joi.string().min(1).max(150).required()
+    });
+
+    const schemaUpdateEmail = joi.object({
+        correo_user: joi.string().min(1).max(70).email().required()
+    });
+
+    const schemaUpdatePassword = joi.object({
+        password_user: joi.string().min(1).max(150).required()
+    });
+
+    const schemaUpdateRecovery = joi.object({
+        recovery_code_user: joi.string().min(1).max(50).required()
+    });
+
+    const schemaUpdateStatus = joi.object({
+        activo_user: joi.boolean().required()
+    });
+
+    return {
+        schemaContentLetters,
+        schemaContentNumbers,
+        schemaCreateUser,
+        schemaUpdateUser,
+        schemaUpdateDataGeneral,
+        schemaUpdateEmail,
+        schemaUpdatePassword,
+        schemaUpdateRecovery,
+        schemaUpdateStatus,
+    }
+})();
+
+module.exports = schemas;
